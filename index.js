d3c55e3328ca3010ecb65641c7f86cd61261feb9
Sean Huang <hyprul@ucla.edu>
2018-08-26 17:00:58 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+
+
+function distanceFromHqInBlocks(location){
+	return location > 42 ? location - 42 : 42 - location
+
+}
+
+
+function distanceFromHqInFeet(block){
+	return distanceFromHqInBlocks(block)*264
+
+}
+
+function distanceTravelledInFeet(beginblock, endblock){
+	return endblock > beginblock ? (endblock - beginblock)*264 : (beginblock - endblock)*264
+}
+
+
+function calculatesFarePrice(begin, end){
+	let distance = distanceTravelledInFeet(begin, end)
+
+	if (distance <= 400){
+		return 0
+	}else if (distance <= 2000){
+		return (distance-400)*.02
+	}else if (distance <= 2500){
+		return 25
+	}else {
+		return 'cannot travel that far'
+	}
+
+}

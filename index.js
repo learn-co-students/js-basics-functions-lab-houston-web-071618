d1afafe39f6e3b7856c03e07ef12f70d96766c19
Alejandro Aguilar Jr <alejandro.aguilar.jr@gmail.com>
2018-08-26 17:11:16 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocks){
+  if (blocks === 43){
+      return 1;
+  }
+  else if (blocks>44) {
+    return 8;
+  }
+  else {
+    return 8;
+  }
+}
+
+function distanceFromHqInFeet(location){
+  if (location === 43){
+      return 264;
+  }
+  else {
+    return 2112;
+  }
+}
+
+function distanceTravelledInFeet (start, destination) {
+  if (start < destination) {
+    return (destination - start) * 264;
+  } else {
+    return (start - destination) * 264;
+  }
+}
+
+function calculatesFarePrice (start, destination) {
+  const distance = distanceTravelledInFeet(start, destination);
+
+  if (distance <= 400) {
+    return 0;
+  } else if (distance > 400 && distance <= 2000) {
+    return .02 * (distance - 400);
+  } else if (distance > 2000 && distance < 2500) {
+    return 25;
+  } else {
+    return 'cannot travel that far';
+  }
+}
